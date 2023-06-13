0e075b239897198e310d787076ae79790dcf159d
RonakSingh98 <129096563+RonakSingh98@users.noreply.github.com>
2023-06-13 11:45:06 +0530

    Add files via upload

---
+++ final_project/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const session = require("express-session");
+const customer_routes = require("./router/auth_users.js").authenticated;
+const genl_routes = require("./router/general.js").general;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(
+  "/customer",
+  session({
+    secret: "fingerprint_customer",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use("/customer/auth/*", function auth(req, res, next) {
+  if (req.session.authorization) {
+    // get the session token
+    token = req.session.authorization["accessToken"];
+    // verify the token
+    jwt.verify(token, "access", (err, user) => {
+      if (!err) {
+        req.user = user;
+        next();
+      } else {
+        return res.status(403).json({ message: "The toke has been expired" });
+      }
+    });
+  } else {
+    return res.status(403).json({ message: "User not logged in" });
+  }
+});
+
+const PORT = 5000;
+
+app.use("/customer", customer_routes);
+app.use("/", genl_routes);
+
+app.listen(PORT, () => console.log("Server is running"));
